632835ef00961e5ccef9fcf8ba2409b726c9e378
nicolkrit999 <krit.nicol.pio@pm.me>
2025-11-18 11:57:30 +0100

    added comments

--- flake.nix
+++ flake.nix
@@ -1,35 +1,45 @@
 {
-  description = "A complete, declarative macOS setup with Neovim for Java";
+  description = "A complete, declarative macOS setup with Neovim, home manager with aliases";
 
+  # Define from where the packages are installed
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # Define that all installed packages are from the unstable channel (the more update one)
+    nix-darwin.url = "github:LnL7/nix-darwin"; # Nix-darwing url fetch
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs"; # Define that nix-darwin uses the regular nix packages
+
+    # Home-manager module support
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
+  # Define outputs
   outputs = { self, nixpkgs, nix-darwin, home-manager }: let
-    system = "aarch64-darwin";
+    system = "aarch64-darwin"; # Define that the system is aarch64 based on darwin
   in {
+    # Define hostname
     darwinConfigurations."Krits-MacBook-Pro" = nix-darwin.lib.darwinSystem {
       specialArgs = { };
+
+      # Define nix enabled modules and various options
       modules = [
         ({ pkgs, lib, ... }: {
           nixpkgs.hostPlatform = system;
-          nixpkgs.config.allowUnfree = true;
+          nixpkgs.config.allowUnfree = true; # Allow allowUnfree packages
+
+          # Allow insecured packages, for example older not maintained version
           nixpkgs.config.permittedInsecurePackages = [
           ];
           programs.nix-index.enable = true;
-          nix.settings.experimental-features = [ "nix-command" "flakes" ];
+          nix.settings.experimental-features = [ "nix-command" "flakes" ]; # Define experimental feature, enable support for flakes and cli nix commands
           ids.gids.nixbld = 350;
-          environment.systemPath = [ "/opt/homebrew/bin" "/opt/homebrew/sbin" ];
-          environment.variables.JAVA_HOME = "${pkgs.jdk25}";
-          environment.variables.JDTLS_BIN = "${pkgs.jdt-language-server}/bin/jdtls";
+          environment.systemPath = [ "/opt/homebrew/bin" "/opt/homebrew/sbin" ]; # Define homebrew as systemPath
+          environment.variables.JAVA_HOME = "${pkgs.jdk25}"; # Set the installed java packages in the environment variable JAVA_HOME. Useful to take it in setup like neovim
+          environment.variables.JDTLS_BIN = "${pkgs.jdt-language-server}/bin/jdtls"; # Override default jdtls, for example to tell neovim to use the installed one instead of installing an older version
 
 
+          # General nix packages installation
           environment.systemPackages = with pkgs; [
             neovim git nodejs maven gradle ripgrep fd unzip eza btop jq
             cmake docker glew glfw pandoc postgresql_14 rbenv redis sox
@@ -46,6 +56,8 @@
             vimPlugins.nvim-treesitter-parsers.regex texliveFull
             vimPlugins.nvim-java-test nerd-fonts.jetbrains-mono
             jdt-language-server
+
+            # Define options for python3.13
             (pkgs.python313.withPackages (ps: with ps; [
               pip
               setuptools
@@ -60,6 +72,7 @@
 
           ];
 
+          # Tell zsh to eval the necessary packages
           programs.zsh = {
             enable = true;
             enableCompletion = true;
@@ -68,6 +81,7 @@
           };
 
 
+          # Define installed homebrew packages
           homebrew = {
             enable = true;
             onActivation = { autoUpdate = false; upgrade = false; cleanup = "uninstall"; };
@@ -77,6 +91,7 @@
             user = "krit";
           };
 
+          # Define the path of the home user. It need to match the related section in home manager (home.nix)
           users.users.krit.home = "/Users/krit";
           security.pam.services.sudo_local.enable = true;
           system.primaryUser = "krit";
@@ -86,6 +101,8 @@
             nixpush = "cd /etc/nix-darwin/ && sudo nix run nix-darwin -- switch --flake .#Krits-MacBook-Pro";
           };
         })
+
+        # Define general home manager behaviour
         home-manager.darwinModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
--- home.nix
+++ home.nix
@@ -1,24 +1,29 @@
+# Introduzione, add if you need other components
 { pkgs, lib, ... }:
 
 {
-  home.username = "krit";
-  home.homeDirectory = "/Users/krit";
-  home.stateVersion = "24.05";
+  home.username = "krit"; # Need to match the current user name
+  home.homeDirectory = "/Users/krit"; # Need to match the current user directory path, the same as ~
+  home.stateVersion = "24.05"; # Nix general version
 
-  programs.home-manager.enable = true;
+  programs.home-manager.enable = true; # Enable home manager module
 
+  # Define home environment variables for the current user
   home.sessionVariables = {
   JAVA_HOME = "${pkgs.jdk25}";
   JDTLS_BIN = "${pkgs.jdt-language-server}/bin/jdtls";
   };
 
 
+  # Define the fact that the zsh configuration is defined in nix. ~/.zshrc is an alias and not the actual file
   programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
+    enable = true; # Needed to allow nix to manage the zshrc
+    enableCompletion = true; # Enable zsh completeation. It means when you press tab there is command/path/etc completetion (autosuggestion)
+    autosuggestion.enable = true; # Enable autosuggestion (completetion) of command based on the history. You can accept the autosuggestion by pressing →
+    syntaxHighlighting.enable = true; # Highlight command sintax allowing you to understand more clearly the different sections of the command
 
+    # Replace all the conent of zshrc with the current one. This get loaded before the entire home manager. In the case this is not a wanted behaviour then initExtra is a better option
+    # This block define homebrew path (mac specific), the iterm integration (mac specific), catpuccin mocha terminal theme, pokemon colorscripts when opening a new terminal
     initContent = ''
       export CASE_SENSITIVE="true"
       export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
@@ -47,17 +52,24 @@
   };
 
 
-  programs.command-not-found.enable = false;
+  programs.command-not-found.enable = false; # Disable auto suggestion of nix to install a certain package/program if the runned command is not found
 
+  # Define the zshr aliases
   home.shellAliases = {
-    upd        = "brew update && brew upgrade";
-    updres     = "brew update-reset";
-    inst       = "brew install";
-    instcask   = "brew install --cask";
-    search     = "brew search";
-    brewcache  = "brew cleanup";
-    ollamaadvanced = "ollama run llama3:70b";
-    ollamabasic    = "ollama run llama3:8b";
+
+    # Brew aliaes (not needed because nix define brew packages)
+    #brew-upd        = "brew update && brew upgrade";
+    #brew-upd-res     = "brew update-reset";
+    #brew-inst       = "brew install";
+    #brew-inst-cask   = "brew install --cask";
+    #brew-search     = "brew search";
+    #brew-clean  = "brew cleanup";
+
+    # Ollama cli run (not needed because it's not installed)
+    #ollama-advanced = "ollama run llama3:70b";
+    #ollama-basic    = "ollama run llama3:8b";
+
+    # Built in command improvement (require eza to be installed)
     sudo = "sudo ";
     l   = "eza -lh --icons=auto";
     ls  = "eza -1  --icons=auto";
@@ -74,7 +86,6 @@
     zshrc = "source ~/.zshrc";
     reb   = "sudo reboot";
     shut  = "sudo shutdown -h now";
-    caff  = "caffeinate";
     del   = "sudo rm -r";
     cp    = "cp -i";
     mkdir = "mkdir -p";
@@ -85,35 +96,57 @@
     ".4" = "cd ../../../..";
     ".5" = "cd ../../../../..";
     ".6" = "cd ../../../../../..";
+
+    # Caffeinate (mac specific)
+    caff  = "caffeinate";
+
+    # Nas wake on lan setup
     wakenas-local = "wakeonlan 6c:1f:f7:40:1e:d6";
-    wakenas-remotely = "wakeonlan -i 46.127.88.190 -p 9 6c:1f:f7:40:1e:d6";
+    #wakenas-remotely = "wakeonlan -i 46.127.88.190 -p 9 6c:1f:f7:40:1e:d6";
+
+    # Useful directory navigation
     downloads = "cd ~/Downloads";
     config    = "cd ~/.config";
-    share     = "cd /opt/homebrew/bin";
-    opt       = "cd /opt/homebrew/opt";
+    share     = "cd ~/.local/share/";
+    opt       = "cd /opt/";
+
+    # Useful git aliases
     clone        = "git clone";
     clonedepth1  = "git clone --depth=1";
     gitkeep      = "find . -type d -empty -not -path './.git/*' -exec touch {}/.gitkeep \\;";
     gac = "find /Users/krit/0001_Github_Repos -name '.git' -type d | while read gitdir; do repo=$(dirname \"$gitdir\"); echo \"Processing: $repo\"; (cd \"$repo\" && git add . && git commit -m \"General-$(date +%Y-%m-%d)\" && git push) 2>/dev/null || echo \"Failed: $repo\"; done";
-    setup     = "cd ~/0001_Github_Repos/0001_MacOS_Setup";
-    personal  = "cd ~/0001_Github_Repos/0002_Personal_repos";
-    work      = "cd ~/0001_Github_Repos/0003_Work";
-    various   = "cd ~/0001_Github_Repos/0004_Various";
+
+
+    # Other commands
     youtube   = "yt-dlp -f bestvideo+bestaudio";
-    htop      = "sudo htop";
+    btop      = "sudo btop";
+
+
+    # Accept xcode license
     xcodeaccept = "sudo xcodebuild -license accept";
+
+    # Various ssh access
     sshtailscale = "ssh krit@nicol-nas";
     sshnasip     = "ssh krit@192.168.1.98";
     sshos        = "ssh -l kritpio.nicol@supsi.ch linux1-didattica.supsi.ch";
     nas-ssh    = "cloudflared access ssh --hostname ssh.nicolkrit.ch";
-    nasdockerubuntucli = "sudo docker exec -it Ubuntu-Krit-admin-dc /bin/bash";
+
+    # Find available docker subnet
     dockersubnetcheck  = "sudo docker network ls --format 'table {{.Name}}\\t{{.Driver}}' && echo '' && sudo docker network ls --format '{{.Name}}' | xargs -I {} sudo docker network inspect {} --format '{{.Name}}: {{range .IPAM.Config}}{{.Subnet}} {{end}}' 2>/dev/null || echo 'Error inspecting networks'";
+
+    # Various dns utilities
     changehosts = "sudo nvim /etc/hosts";
     cleardns    = "sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder";
+
+    # Nix specific aliases
     nixpush = "cd /etc/nix-darwin/ && sudo nix run nix-darwin -- switch --flake .#Krits-MacBook-Pro";
+
+    # Other directories navigation
     javanvim = "cd /Users/krit/developing-projects/java-projects/ && nvim";
     pythonnvim = "cd /Users/krit/developing-projects/python-projects/ && nvim";
     developingnvim = "cd /Users/krit/developing-projects && nvim";
+
+    # Maven rebuild for java projects
     rebuildmvn = "cd ~/java-projects && mvn clean install";
   };
 }
