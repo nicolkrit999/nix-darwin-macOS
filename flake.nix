ae8cbe4ad3e46ea89e5c1b726e9c68fd5210e3ea
Krit Pio Nicol <krit.nicol.pio@pm.me>
2025-12-15 01:41:15 +0100

    lol

--- flake.nix
+++ flake.nix
@@ -2,24 +2,16 @@
   description = "Krit's Nix-Darwin System Config";
 
   inputs = {
-    # ✅ Stable 25.11 branches
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
-
     nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Theming
     stylix.url = "github:danth/stylix/release-25.11";
-
     catppuccin.url = "github:catppuccin/nix";
     catppuccin.inputs.nixpkgs.follows = "nixpkgs";
-
     firefox-addons.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
     firefox-addons.inputs.nixpkgs.follows = "nixpkgs";
-
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -36,29 +28,28 @@
     }@inputs:
     let
       # ---------------------------------------------------------
-      # ⚙️ GLOBAL VARIABLES
+      # ⚙️ SYSTEM DEFINITION WRAPPER
       # ---------------------------------------------------------
-      user = "krit";
-      system = "aarch64-darwin";
-
-      # 🎨 THEME SETTINGS
-      globalBase16Theme = "catppuccin-macchiato";
-      globalPolarity = "dark";
-
-      # 🐱 CATPPUCCIN TOGGLES
-      catppuccinEnable = true;
-      catppuccinFlavor = "macchiato";
-      catppuccinAccent = "mauve";
-
+      # We removed the hardcoded vars here. Now they are arguments:
       mkSystem =
-        { hostname, monitorConfig }:
+        {
+          hostname,
+          monitorConfig,
+          user, # <--- Added argument
+          base16Theme, # <--- Added argument
+          polarity, # <--- Added argument
+          catppuccinEnable, # <--- Added argument
+          catppuccinFlavor, # <--- Added argument
+          catppuccinAccent, # <--- Added argument
+        }:
         nix-darwin.lib.darwinSystem {
-          inherit system;
+          system = "aarch64-darwin"; # Can also be an arg if you have Intel macs
           specialArgs = { inherit inputs user; };
           modules = [
             ./modules/darwin/default.nix
-            ./modules/darwin/stylix.nix
-            stylix.darwinModules.stylix
+
+            # 🔴 REMOVED: ./modules/darwin/stylix.nix
+            # (This file belongs in Home Manager because it themes user apps like bat/lazygit)
 
             # Import Host Specific Packages
             ./hosts/${hostname}/local-packages.nix
@@ -78,12 +69,12 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
 
-              # Pass variables to Home Manager modules
+              # Pass the SPECIFIC variables to Home Manager modules
               home-manager.extraSpecialArgs = {
                 inherit inputs user;
-                base16Theme = globalBase16Theme;
-                polarity = globalPolarity;
-                catppuccin = catppuccinEnable;
+                base16Theme = base16Theme; # <--- Uses the arg passed to mkSystem
+                polarity = polarity; # <--- Uses the arg passed to mkSystem
+                catppuccin = catppuccinEnable; # <--- Uses the arg passed to mkSystem
                 inherit catppuccinFlavor catppuccinAccent;
                 monitors = monitorConfig;
                 gitUserName = "nicolkrit999";
@@ -95,6 +86,9 @@
                   ./modules/home-manager/default.nix
                   inputs.catppuccin.homeManagerModules.catppuccin
                   inputs.nix-index-database.hmModules.nix-index
+
+                  # 🟢 ADDED HERE:
+                  ./modules/darwin/stylix.nix
                 ];
                 home.stateVersion = "24.05";
               };
@@ -102,23 +96,32 @@
           ];
         };
     in
-
     {
       darwinConfigurations = {
         # 💻 Configuration 1: MacBook Pro (Krit)
-        # Native Resolution for 14" M3 Pro is 3024x1964
-        # Triggers Font Size 16.0 in Alacritty
         "Krits-MacBook-Pro" = mkSystem {
           hostname = "Krits-MacBook-Pro";
           monitorConfig = [ "eDP-1,3024x1964,1" ];
+          # Custom settings for this host:
+          user = "krit";
+          base16Theme = "catppuccin-macchiato";
+          polarity = "dark";
+          catppuccinEnable = true;
+          catppuccinFlavor = "macchiato";
+          catppuccinAccent = "mauve";
         };
 
         # 💻 Configuration 2: MacBook Air (Roberta)
-        # Native Resolution for 13" M2 Air is 2560x1664
-        # Triggers Font Size 13.0 in Alacritty
         "MacBook-Air-di-Roberta" = mkSystem {
           hostname = "MacBook-Air-di-Roberta";
           monitorConfig = [ "eDP-1,2560x1664,1" ];
+          # Roberta might want a different theme/user:
+          user = "roberta"; # <--- Different user!
+          base16Theme = "catppuccin-latte"; # <--- Light mode example
+          polarity = "light";
+          catppuccinEnable = true;
+          catppuccinFlavor = "latte";
+          catppuccinAccent = "blue";
         };
       };
     };
