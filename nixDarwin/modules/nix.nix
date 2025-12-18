423374455bbc4333f1531b33674e8a7b71cbefb0
Krit Pio Nicol <krit.nicol.pio@pm.me>
2025-12-18 09:36:31 +0100

    minor garbage collection refactoring

--- nixDarwin/modules/nix.nix
+++ nixDarwin/modules/nix.nix
@@ -9,23 +9,5 @@
         "flakes"
       ];
     };
-
-    # ♻️ GARBAGE COLLECTION
-    # 🔴 MUST BE FALSE because 'nix.enable = false'
-    gc = {
-      automatic = false;
-
-      # We keep these settings here for reference,
-      # but they won't run automatically on this system.
-      interval = {
-        Weekday = 0;
-        Hour = 0;
-        Minute = 0;
-      };
-      options = "--delete-older-than 7d";
-    };
-
-    # 🔴 MUST BE FALSE because 'nix.enable = false'
-    optimise.automatic = false;
   };
 }
